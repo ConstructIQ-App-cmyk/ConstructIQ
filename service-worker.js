fce42136c743075e4904254bd41f396a198f1f7f
ConstructIQ-App-cmyk <loganklaiber@icloud.com>
2026-07-30 18:06:15 -0400

    Add live operations reports

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'current-v43';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js?v=43', './manifest.webmanifest', './icon.svg'];
+const CACHE_NAME = 'current-v44';
+const APP_SHELL = ['./', './index.html', './styles.css', './app.js?v=44', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
